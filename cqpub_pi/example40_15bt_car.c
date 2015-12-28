b8b2e02dec05c55f12ab17da83f223738991f91b
Wataru KUNINO <xbee@dream.jp>
2015-12-28 19:22:31 +0900

    add termios termKey

--- cqpub_pi/example40_15bt_car.c
+++ cqpub_pi/example40_15bt_car.c
@@ -11,52 +11,59 @@ int main(){
     char c;                                         // キー入力用の文字変数c
     int loop=1;                                     // 変数loop(0:ループ終了)
     
+    struct termios termKey,termKey_bk;              // キー入力のエコーを止める処理
+    tcgetattr(0, &termKey);
+    tcgetattr(0, &termKey_bk);
+    termKey.c_lflag &= ~ECHO;
+    tcsetattr(0, TCSANOW, &termKey);
+    
     printf("example 40 Bluetooth Motor Car for IchigoJam\n");
     if(open_rfcomm(mac) <= 0){                      // Bluetooth接続の開始
         printf("Bluetooth Open ERROR\n");
         return -1;
     }
     printf("CONNECTED\n[LEFT] <-> [RIGHT], [UP]:Speed, [DOWN]:Slow [q]:Quit\n");
-    write(ComFd, "\x1b\x10 CLS\n", 7);              // IchigoJamの画面制御
     while(loop){
         if(kbhit()){                                // キーボードから入力があった時
             c=getchar();                            // 入力された文字を変数cへ代入
             switch(c){
                 case 0x44:                          // 「左」が入力された時
-                    printf("[LEFT]");
+                    printf("[L]");
                     c=(char)28;                     // IchigoJam文字コード 28
                     break;
                 case 0x43:                          // 「右」が入力された時
-                    printf("[RIGHT]");
+                    printf("[R]");
                     c=(char)29;                     // IchigoJam文字コード 29
                     break;
                 case 0x41:                          // 「上」が入力された時
-                    printf("[UP]");
+                    printf("[U]");
                     c=(char)30;                     // IchigoJam文字コード 30
                     break;
                 case 0x42:                          // 「下」が入力された時
-                    printf("[DOWN]");
+                    printf("[D]");
                     c=(char)31;                     // IchigoJam文字コード 31
                     break;
                 case '\n':                          // 「Enter」が入力された時
-                    printf("[ANA]");
+                    printf("\n[ANA]\n");
                     c=0;                            // 方向入力では無い
                     write(ComFd, "\x1b\x10", 2);    // エスケープを送信する
                     usleep(50000);                  // 50msの(IchigoJam処理)待ち時間
                     write(ComFd, "?ANA(2)\n",8);    // IchigoJamへ「?ANA(2)」を送信する
                     break;
                 case ' ':                           // 「スペース」が入力された時
-                    printf("[LRUN]");
+                    printf("\n[LRUN]\n");
                     c=0;                            // 方向入力では無い
                     write(ComFd, "\x1b\x10", 2);    // エスケープを送信する
                     usleep(50000);                  // 50msの(IchigoJam処理)待ち時間
                     write(ComFd, "LRUN0\n",6);      // IchigoJamへ「LRUN0」を送信する
                     break;
                 case 'q': case 'Q':                 // 「q」が入力された時
+                    printf("\n[QUIT]\n");
                     c=0;                            // 方向入力では無い
+                    write(ComFd, "\x1b\x10", 2);    // エスケープを送信する
                     for(loop=3;loop>0;loop--){
+                        usleep(200000);             // 200msの(IchigoJam処理)待ち時間
                         write(ComFd, "OUT0\n",5);   //「OUT0」を3回送信
-                        sleep(1);
                     }                               // whileを抜けるためにloopを0に設定
                     break;
                 default:
@@ -68,6 +75,7 @@ int main(){
         if(c) printf("%c",c);                       // 受信を表示
     }
     printf("\nDONE\n");
+    tcsetattr(0, TCSANOW, &termKey_bk);
     close_rfcomm();
     return 0;
 }
