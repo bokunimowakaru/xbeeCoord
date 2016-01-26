1047e34488ff840038fb3cf76a849b1cfa58cb16
Wataru KUNINO <xbee@dream.jp>
2016-01-26 20:36:19 +0900

    取得時刻の変更。室温取得コマンドの変更

--- cqpub_pi/example50_temp.c
+++ cqpub_pi/example50_temp.c
@@ -31,8 +31,8 @@ int main(int argc,char **argv){
     while(1){
         time(&timer);                               // 現在の時刻を変数timerに取得する
         time_st = localtime(&timer);                // timer値を時刻に変換してtime_stへ
-        if( timer%10 == 9 || trig ==0 ){            // 10秒毎、または Trigが0の時
-            len = bt_cmd("\x1b");                   // Arduinoへ室温を問い合わせる
+        if( timer%10 == 0 || trig ==0 ){            // 10秒毎、または Trigが0の時
+            len = bt_cmd("\n\x1b");                 // Arduinoへ室温を問い合わせる
             if(len) temp[0]=atoi(rx_data);          // 受信した室温をtemp[0]に保持する
             strftime(s,17,"%H:%M",time_st);         // 文字列変数sに時刻を代入
             sprintf(s,"%s %d / %d",s,temp[1],temp[2]);  // 文字列変数sに温度を追加
