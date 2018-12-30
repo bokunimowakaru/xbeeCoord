e64e2ebebe705642462fdfc6d7942713f3dc057b
Wataru KUNINO <xbee@dream.jp>
2018-12-30 19:06:49 +0900

    電池電圧を追加

--- examples_zb3/basic03_temp.py
+++ examples_zb3/basic03_temp.py
@@ -1,17 +1,18 @@
 # MicroPython XBee3 ZigBee
 # coding: utf-8
 '''
-内蔵温度センサの値を読み取る
+内蔵温度センサと電源電圧の値を読み取る
                                                   Copyright (c) 2018-2019 Wataru KUNINO
 '''
 import xbee
 import sys, time
-TEMP_OFFSET=15.0			# 内部温度上昇
+TEMP_OFFSET=14.0			# 内部温度上昇
 
 def temp():
-	ret=xbee.atcmd('TP') - TEMP_OFFSET
-	return ret
+	temp = xbee.atcmd('TP') - TEMP_OFFSET
+	volt = xbee.atcmd('%V') / 1000
+	return {'temp':temp, 'volt':volt}
 
 while True:
 	temp()
-	time.sleep_ms(1000)
+	time.sleep_ms(3000)
